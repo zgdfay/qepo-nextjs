9d486d8a9c1ec3366a21d5c36a95f80cd9ec66fb
zgdfay <sinyoxera@gmail.com>
2025-02-01 11:38:49 +0700

    init project real

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,20 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { ThemeProvider } from "~/components/theme-provider";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <div className={GeistSans.className}>
-      <Component {...pageProps} />
-    </div>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <div className={GeistSans.className}>
+        <Component {...pageProps} />
+      </div>
+    </ThemeProvider>
   );
 };
 
